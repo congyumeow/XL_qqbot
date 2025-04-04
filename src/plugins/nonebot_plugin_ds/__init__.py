ef28ab640a0e4cc66f5ad0c4ccf972367e1d0e3c
congyumeow <2673377454@qq.com>
2025-04-04 16:01:02 +0800

    deepseek chat plugin

---
+++ src/plugins/nonebot_plugin_ds/__init__.py
@@ -0,0 +1,66 @@
+import nonebot
+from openai import OpenAI
+from nonebot import on_message
+from nonebot.adapters.qq import Event, MessageEvent
+from nonebot.rule import Rule
+from nonebot.plugin import PluginMetadata
+
+from .config import base_url, api_key, setting
+
+__plugin_meta__ = PluginMetadata(
+    name="deepseek-chat",
+    description="deepseek-chat",
+    usage="deepseek-chat",
+    extra={
+        "author": "congyumeow <l72221112@gmail.com>",
+        "version": "v1.0.0"
+    }
+)
+
+DEEPSEEK_API_URL = base_url
+DEEPSEEK_API_KEY = api_key
+SETTING = setting
+
+COMMAND_LIST = [
+    cmd for cmd in nonebot.get_driver().config.command_start
+]
+
+def get_ds_response(user_msg: str):
+    client = OpenAI(api_key=DEEPSEEK_API_KEY, base_url=DEEPSEEK_API_URL)
+
+    completion = client.chat.completions.create(
+        model="deepseek-chat",
+        messages=[
+            {
+                "role": "system",
+                "content": f"你是{settings}，用口语化中文回答。"
+            },
+            {
+                "role": "user",
+                "content": user_msg
+            }
+        ]
+    )
+
+    return completion.choices[0].message.content
+
+
+async def is_normal_messagge(event: MessageEvent) -> bool:
+    text = event.get_plaintext().strip()
+    return not any(
+        text.startswith(cmd)
+        for cmd in COMMAND_LIST
+    )
+
+
+ds_chat = on_message(
+    priority=10,
+    rule=Rule(is_normal_messagge)
+)
+
+
+@ds_chat.handle()
+async def _(event: MessageEvent):
+    user_msg = event.get_plaintext().strip()
+    response = get_ds_response(user_msg)
+    await ds_chat.send(response)
---
+++ src/plugins/nonebot_plugin_ds/config.py
@@ -0,0 +1,5 @@
+base_url = "https://api.deepseek.com"
+api_key = "your_api_key"
+setting = """
+    
+"""
