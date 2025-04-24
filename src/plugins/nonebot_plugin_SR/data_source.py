5cf1dce840a5637e9f17951f118a4512b00f30f2
congyumeow <2673377454@qq.com>
2025-04-25 00:20:55 +0800

    fix

--- src/plugins/nonebot_plugin_SR/data_source.py
+++ src/plugins/nonebot_plugin_SR/data_source.py
@@ -169,8 +169,7 @@ def get_gacha_data(authkey):
         page = 1
         end_id = 0
         while True:
-            url = re.sub(r'(gacha_type=)\d+', r'\g<1>{}'.format(gachaType), url)
-            url = re.sub(r'page=(.*?)&', "page={}&".format(page), url)
+            url = re.sub(r'gacha_type=(.*?)&', r'gacha_type={}&'.format(gachaType), url)
             url = re.sub(r'end_id=(.*?)\d+', "end_id={}".format(end_id), url)
             response = requests.get(url, headers=heards)
             body = response.text
